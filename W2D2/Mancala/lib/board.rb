361c3aff3eb7813580e5d6009d8221db8ef73003
Howard Feger <hfeger4@gmail.com>
2017-06-11 18:58:58 -0700

    updated mancala W2D2

--- W2D2/Mancala/lib/board.rb
+++ W2D2/Mancala/lib/board.rb
@@ -2,57 +2,53 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
-    @name1 = name1
-    @name2 = name2
+    @name1, @name2 = name1,name2
     @cups = Array.new(14){Array.new}
     place_stones
   end
 
   def place_stones
-    @cups.each do |cup|
-      4.times do
-        cup << :stone
+    # helper method to #initialize every non-store cup with four stones each
+    @cups.each_with_index do |cup,i|
+      unless i == 6 || i == 13
+        4.times do
+          cup << :stone
+        end
       end
     end
-    @cups[6] = []
-    @cups[13] = []
   end
 
   def valid_move?(start_pos)
-    raise 'Invalid starting cup' if start_pos > 12 || start_pos < 0
-    raise 'Invalid starting cup' if @cups[start_pos].empty?
+    raise "Invalid starting cup" if start_pos > 14 || start_pos <= 0
   end
 
   def make_move(start_pos, current_player_name)
-    if current_player_name == @name1
-      i = 0
-      until @cups[start_pos].length <= 0
-      ending_cup_idx = start_pos + i
-        i += 1 if start_pos+i == 13
-        @cups[(start_pos+i) % @cups.length] << @cups[start_pos].pop
-        i += 1
-      end
-    else
-      i = 0
-      until @cups[start_pos].length <= 0
-      ending_cup_idx = start_pos + i
-        i += 1 if start_pos+i == 0
-        @cups[(start_pos+i) % @cups.length] << @cups[start_pos].pop
-        i += 1
+    stones = @cups[start_pos]
+    cup_idx = start_pos
+    until stones.empty?
+      cup_idx += 1
+      cup_idx = 0 if cup_idx > 13
+      if cup_idx == 6
+        @cups[6] << stones.pop if current_player_name == @name1
+      elsif cup_idx == 13
+        @cups[13] << stones.pop if current_player_name == @name2
+      else
+        @cups[cup_idx] << stones.pop
       end
     end
-
     render
-    next_turn(ending_cup_idx)
+    next_turn(cup_idx)
   end
 
   def next_turn(ending_cup_idx)
-    case ending_cup_idx
-    when @cups[ending_cup_idx].empty?
-      move_result = :switch
+    # helper method to determine what #make_move returns
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt
+    elsif @cups[ending_cup_idx].length == 1
+      :switch
+    else
+      ending_cup_idx
     end
-
-
   end
 
   def render
@@ -64,12 +60,15 @@ class Board
   end
 
   def one_side_empty?
-
+    return true if @cups[0..6].all?{|cup| cup.empty?}
+    return true if @cups[7..12].all?{|cup| cup.empty?}
+    false
   end
 
   def winner
+    player1_points = @cups[6].length
+    player2_points = @cups[13].length
+    return :draw if player1_points == player2_points
+    player1_points > player2_points ? @name1 : @name2 
   end
 end
-
-a = Board.new("a","b")
-p a
